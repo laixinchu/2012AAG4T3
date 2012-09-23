fb0b44d764e00db46a103117afd3256c69198bab
Kel4416 <Kel4416@Khleong2010>
2012-09-23 16:45:40 +0800

    test

---
+++ src/java/logger/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logger;
+
+/**
+ *
+ * @author Kel4416
+ */
+public class HelloWorld {
+    
+}
